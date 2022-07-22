3781cd7d0de39c58b914c930bf0ffba9dceeec15
Yu Min Seung <90673332+yms2@users.noreply.github.com>
2022-07-22 12:52:49 +0900

    Add files via upload

--- src/content.js
+++ src/content.js
@@ -4,9 +4,23 @@ function Content(){
     return(
         <div className="content2">
             <div className="buy">
-            <img src="https://images.samsung.com/kdp/cms_contents/151281/be3a3d59-03aa-4adf-83e1-bb9661364118.jpg"/>
-            <img src="https://images.samsung.com/kdp/cms_contents/133195/b989d65e-4e08-4f03-8766-1a8e20bc1ef3.jpg" />
-            <img src="https://images.samsung.com/kdp/cms_contents/151281/6474b798-ac02-4d36-8841-d157591d203b.jpg"/>
+            <div className="img1">
+            <h2>Galaxy S22 Series</h2>
+            <p>더욱 강력해진 갤럭시를 삼성카드 결제시 최대 29만원 상당 혜택으로 만나보세요</p>
+            <span><a href="#"> 더 알아보기</a><button>구매하기</button></span>
+            </div>
+            <div className="img2">
+            <h2>Neo QLED</h2>
+            <p>Global No.1이 만든 새로운 화질의 시작</p>
+            <span><a href="#"> 더 알아보기</a><button>제품 모두 보기</button></span>
+            </div>
+            <div className="img3">
+            <h2>BESPOKE 무풍에어컨</h2>
+            <p>무풍에어컨으로 하루종일 기분 좋은 시원함을 느껴보세요</p>
+            <span>
+            <button>제품 모두 보기</button>
+            </span>
+            </div>
             </div>
         </div>
     )
